f1f4b4cf56f5bf57693da5f42000bc68f444e535
Richard Campos <richardfcampos@gmail.com>
2021-02-24 22:14:16 -0300

    add client

--- client/src/app.js
+++ client/src/app.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import PostCreate from "./PostCreate";
-import PostList from "./PostList";
+import React from 'react';
+import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 export default () => {
-    return (
+  return (
     <div className="container">
-        <h1>Create Post</h1>
-        <PostCreate />
-        <hr />
-        <h1>Posts</h1>
-        <PostList />
-    </div>)
-}
+      <h1>Create Post</h1>
+      <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
+    </div>
+  );
+};
