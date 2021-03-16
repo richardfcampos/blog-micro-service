70bbab944dc7769849793b4685644df961a1e75d
Richard Campos <richardfcampos@gmail.com>
2021-03-16 18:51:58 -0300

    add moderation service

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,23 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import axios from 'axios';
 
-export default ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchData = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-
-    setComments(res.data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+export default ({ comments }) => {
 
   const renderedComments = comments.map(comment => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content, style
+    switch (comment.status){
+      case 'approved':
+        style = {}
+        content = comment.content
+        break
+      case 'rejected':
+        style = { color: 'red'}
+        content = 'This comment is rejected'
+        break
+      default:
+        style = { color: 'grey'}
+        content = 'This comment is awaiting moderation'
+    }
+
+    return <li key={comment.id} style={style}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
